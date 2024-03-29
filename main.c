9a1dbc88d6b5c9db77192b2f53e9cfee91c0a237
D4kooo <105608882+D4kooo@users.noreply.github.com>
2024-03-29 15:04:33 +0100

    main.c

--- main.c
+++ main.c
@@ -1,88 +1,71 @@
-#include <stdio.h>
+/******************************************************************************
+*  ASR => 4R2.04                                                              *
+*******************************************************************************
+*                                                                             *
+*  N° de Sujet :             Sujet 3                                          *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Intitulé :             Chiffrement de Message                              *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom-prénom1 :          Giner Clément                                       *
+*                                                                             *
+*  Nom-prénom2 :          Autret Pierrick                                     *
+*                                                                             *
+*  Nom-prénom3 :                                                              *
+*                                                                             *
+*  Nom-prénom4 :                                                              *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom du fichier :       main.c                                              *
+*                                                                             *
+******************************************************************************/
+
 #include "cesar.h"
 #include "vigenere.h"
+#include <stdio.h>
+#include <string.h>
 
-/* Fonctions de chiffrement/déchiffrement */
-
-/* Fonctions de menu */
-void menu_cesar() {
-    char message[100];
-    int cle;
-    char cle2[100];
-    int choix = 0;
-    int ChoixTraitement;
-    int ChoixClef;
+int main() {
+    int choix, cleCesar;
+    char message[1024], cleVigenere[256];
     
-  /* Ecriture du message a traiter */
-  printf("Veuillez entrer le message à chiffrer/déchiffrer : ");
-  fgets(message, sizeof(message), stdin);
-  
-  /* Choix du format de la clé, plusieures méthodes de chiffrement peuvent avoir des clés similaires */
-  printf("Choississez le format de votre clé\n");
-  printf("(1) : Clé avec chiffres\n");
-  printf("(2) : Clé avec lettres\n ");
-  scanf("%d", &ChoixClef);
-  switch(ChoixClef) {
-  case 1:
-  /* Ici, on va entrer une clé avec des CHIFFRES, ce qui sous entend qu'elle sera compatible avec toutes les méthodes de chiffrement utilisant des CHIFFRES */
-  printf("Veuillez entrer la clé de chiffrement : ");
-  scanf("%d", &cle);
-  break;
-  case 2:
-  /* Ici, on va entrer une clé avec des CHARACTERES, ce qui sous entend qu'elle sera compatible avec toutes les méthodes de chiffrement utilisant des CHARACTERES */
-  printf("Veuillez entrer la clé de chiffrement : ");
-  scanf("%s", &cle2);
-  break;
-  }
-  
-  
-  /* Choix du type de chiffrement */
-  printf("Que voulez-vous faire ?\n");
-  if (ChoixClef == 1){
-  printf("1. Cesar\n");
-  printf("2. Nous avons detecté que votre clé ne correspondait pas a ce type de format\n");
-  printf("3. Quitter\n");
-  printf("Votre choix : ");
-  }
-  if (ChoixClef == 2){
-  printf("1. Nous avons detecté que votre clé ne correspondait pas a ce type de format\n");
-  printf("2. Vigenère\n");
-  printf("3. Quitter\n");
-  printf("Votre choix : ");
-  }
-  
-  scanf("%d", &choix);
-  switch(choix) {
-    case 1:
-      printf("Voulez vous dechiffrer ou chiffrer ?");
-      ChoixTraitement = 0;
-      scanf("%d", &ChoixTraitement);
-      if (ChoixTraitement==1) {
-      chiffrement_cesar(message, cle);
-      }
-      else
-      {
-      dechiffrement_cesar(message, cle);
-      }
-    break;
-    case 2:
-    printf("voulez vous chiffrer ou déchiffrer ?");
-    ChoixTraitement = 0;
-    scanf("%d", &ChoixTraitement);
-    if (ChoixTraitement==1) {
-        chiffrement_vigenere(message, cle2);
-    }
-    else
-    {
-        dechiffrement_vigenere(message, cle2);
-    }
-      break;
-      case 3:
-      printf("Au revoir !\n");
-      break;
-    }
-}
+    printf("Choisissez l'opération :\n");
+    printf("1. Chiffrement\n");
+    printf("2. Déchiffrement\n");
+    scanf("%d", &choix);
+    getchar(); // Pour consommer le caractère de nouvelle ligne après le nombre
 
-void main() {
-    menu_cesar();
+    printf("Entrez le message : ");
+    gets(message); // Attention à la sécurité de gets dans un vrai programme
+
+    if (choix == 1 || choix == 2) {
+        printf("Choisissez le type de chiffrement :\n");
+        printf("1. César\n");
+        printf("2. Vigenère\n");
+        scanf("%d", &choix);
+        getchar(); // Pour consommer le caractère de nouvelle ligne
+
+        if (choix == 1) { // Chiffrement César
+            printf("Entrez la clé (nombre) : ");
+            scanf("%d", &cleCesar);
+            getchar(); // Pour consommer le caractère de nouvelle ligne
+            if (choix == 1) chiffrement_cesar(message, cleCesar);
+            else dechiffrement_cesar(message, cleCesar);
+        } else if (choix == 2) { // Chiffrement Vigenère
+            printf("Entrez la clé (texte) : ");
+            gets(cleVigenere); // Attention à la sécurité de gets dans un vrai programme
+            if (choix == 1) chiffrement_vigenere(message, cleVigenere);
+            else dechiffrement_vigenere(message, cleVigenere);
+        }
+    } else {
+        printf("Choix non valide.\n");
+        return 1; // Fin prématurée du programme
+    }
+
+    printf("Résultat : %s\n", message);
+    return 0;
 }
